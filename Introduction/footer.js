1dc4129507d4f40a533896f115ddaaf5e5880254
Taihey <taikan0604@gmail.com>
2024-01-09 00:30:37 +0900

    Mini Homeのマニュアルや概要を説明したHTMLファイルです。

---
+++ Introduction/footer.js
@@ -0,0 +1,4 @@
+document.write('        <div><p id="footer">-Mini Home- 菅野泰平 2025年卒</p></div> ')
+document.write('        <script src="nav.js"></script>')
+document.write('    </body> ')
+document.write('</html> ')
